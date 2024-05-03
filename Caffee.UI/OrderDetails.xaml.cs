64aca94cb531b0426f11aca2667c56f590f84f0e
Olena Markiv <zevunze@gmail.com>
2024-05-03 23:41:20 +0300

    cccc

--- Caffee.UI/OrderDetails.xaml.cs
+++ Caffee.UI/OrderDetails.xaml.cs
@@ -6,19 +6,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 namespace Caffee.UI
 {
     /// <summary>
     /// Interaction logic for OrderDetails.xaml
     /// </summary>
-    public partial class OrderDetails : Window
+    public partial class OrderDetails : System.Windows.Window
     {
         private Order _order;
         public OrderDetails()
@@ -28,6 +22,62 @@ namespace Caffee.UI
         public OrderDetails(Order order) : this()
         {
             _order = order;
+            this.IDLabel.Content = order.ID;
+            this.TableNumberLabel.Content = order.Table.ID;
+            this.SeatsNumberLabel.Content = order.Table.SeatsNumber;
+            this.StatusLabel.Content = order.Status.Name;
+            this.CreationTimeLabel.Content = order.CreationTime.ToString();
+            this.WaiterLabel.Content = $"{order.Waiter.Person.FirstName} {order.Waiter.Person.LastName}";
+            this.DiscountLabel.Content = order.Discount is null ? "None :(" : order.Discount.Type.Type == "persents" ? $"{order.Discount.Value}%" : $"{order.Discount.Value}$";
+            double sum = Math.Round(order.OrderDetails.Select(x => x.UnitPrice * x.Amount).Sum(), 2);
+
+            if (order.Discount is not null)
+            {
+                if (order.Discount?.Type.Type == "persents")
+                    sum = (sum / 100.0) * (100.0 - order.Discount.Value);
+                else
+                {
+                    sum = sum - order.Discount.Value;
+                }
+            }
+
+            this.OrderDetailsStackPanel.Children.Clear();
+
+            foreach(var orderDetail in order.OrderDetails)
+            {
+                this.OrderDetailsStackPanel.Children.Add(CreateOrderDetail(orderDetail));
+            }
+
+            this.SumLabel.Content = $"{sum}$";
+        }
+
+        private Grid CreateOrderDetail(OrderDetail orderDetail)
+        {
+            Grid grid = new();
+
+            Label label1 = new Label();
+            label1.Content = orderDetail.Dish.Name;
+            label1.Margin = new Thickness(0, 0, 157, 0);
+
+            Label label2 = new Label();
+            label2.Content = orderDetail.UnitPrice;
+            label2.Margin = new Thickness(136, 0, 121, 0);
+
+            Label label3 = new Label();
+            label3.Content = orderDetail.Amount;
+            label3.Margin = new Thickness(197, 0, 69, 0);
+
+            Label label4 = new Label();
+            label4.Content = Math.Round(orderDetail.Amount * orderDetail.UnitPrice, 2);
+            label4.Margin = new Thickness(244, 0, 0, 0);
+
+            // Додавання Label до контейнера, наприклад, до StackPanel
+            grid.Children.Add(label1);
+            grid.Children.Add(label2);
+            grid.Children.Add(label3);
+            grid.Children.Add(label4);
+
+            return grid;
         }
 
         private void OKButton_Click(object sender, RoutedEventArgs e)
